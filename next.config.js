e1176511f31aadb4d39cb4a6182bc517d517cbf4
greigs-website <greigswebsite@gmail.com>
2026-01-19 21:22:43 +0000

    Add basePath for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -2,5 +2,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: { unoptimized: true },
   output: 'export',
+  basePath: '/greigs-ledger',
+  assetPrefix: '/greigs-ledger/',
 }
 module.exports = nextConfig
